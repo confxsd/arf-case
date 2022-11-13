1e4bcef1580e5ef5bbc43e6661513ba3d1120c7a
serhat <serhat@bxld.space>
2022-11-13 21:30:39 +0300

    add failing test for middleware

---
+++ api/middleware.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"serhatbxld/arf-case/token"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationPayloadKey = "authorization_payload"
+)
+
+// AuthMiddleware creates a gin middleware for authorization
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+	}
+}
