3f2a1e6f0a1ebf6421609085eb5bbd1d4834d456
serhat <serhat@bxld.space>
2022-11-14 21:37:21 +0300

    add failing create offer endpoint test

---
+++ api/offer_test.go
@@ -0,0 +1,102 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	mockdb "serhatbxld/arf-case/db/mock"
+	db "serhatbxld/arf-case/db/sqlc"
+	"serhatbxld/arf-case/token"
+	"serhatbxld/arf-case/util"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateOfferAPI(t *testing.T) {
+	user := randomUser(t)
+	amount := int64(10)
+
+	wallet := randomWallet(user.ID)
+
+	wallet.Currency = util.USD
+	toCurrency := util.TRY
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"from_currency": wallet.Currency,
+				"to_currency":   toCurrency,
+				"amount":        amount,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetWallet(gomock.Any(), gomock.Eq(wallet.ID)).Times(1).Return(wallet, nil)
+
+				arg := db.CreateOfferParams{
+					FromCurrency: wallet.Currency,
+					ToCurrency:   toCurrency,
+					Amount:       amount,
+				}
+				store.EXPECT().CreateOffer(gomock.Any(), gomock.Eq(arg)).Times(1)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchOffer(t, recorder.Body)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := newTestServer(t, store)
+			recorder := httptest.NewRecorder()
+
+			// Marshal body data to JSON
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/offers"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			tc.setupAuth(t, request, server.tokenMaker)
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+
+}
+
+func requireBodyMatchOffer(t *testing.T, body *bytes.Buffer) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotOffer db.Offer
+	err = json.Unmarshal(data, &gotOffer)
+
+	require.NoError(t, err)
+	require.Equal(t, gotOffer.Status, "active")
+}
