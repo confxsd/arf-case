7a4b8a53b0887424b7d6daafa99f516c105aef55
serhat <serhat@bxld.space>
2022-11-15 11:25:21 +0300

    fix ref issues

--- db/migration/000003_add_wallet.up.sql
+++ db/migration/000003_add_wallet.up.sql
@@ -6,4 +6,4 @@ CREATE TABLE "wallets" (
     "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE "wallets" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "wallets" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id")  ON DELETE CASCADE;;
--- db/migration/000004_add_offer.up.sql
+++ db/migration/000004_add_offer.up.sql
@@ -4,8 +4,9 @@ CREATE TABLE "offers" (
     "from_currency" varchar NOT NULL,
     "to_currency" varchar NOT NULL,
     "amount" bigint NOT NULL,
-    "status" varchar NOT NULL DEFAULT "active",
+    "rate" numeric(6) NOT NULL,
+    "status" varchar NOT NULL DEFAULT 'active',
     "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE "offers" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "offers" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
